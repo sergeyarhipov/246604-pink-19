29da917d7094c39b12e1a806d865c305cd252cd7
Сергей Архипов <s.m.arhipov@mail.ru>
2020-05-04 23:28:00 +0500

    Добавлено копирование полифилла в build/js

--- gulpfile.js
+++ gulpfile.js
@@ -70,6 +70,7 @@ gulp.task("copy", function() {
   return gulp.src([
     "source/fonts/**/*.{woff, woff2}",
     "source/img/**",
+    "source/js/picturefill.min.js",
     "source/*.ico"
   ], {
     base: "source"
