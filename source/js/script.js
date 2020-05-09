d53251c04a45c21fae5deb8fb791c283ddc9ce3a
Сергей Архипов <s.m.arhipov@mail.ru>
2020-05-09 23:45:52 +0500

    Сделана навигация без JS

--- source/js/script.js
+++ source/js/script.js
@@ -1,27 +1,22 @@
-/* Панель навигации */
 var nav = document.querySelector(".navigation");
-/* Кнопка-бургер */
 var navToggle = document.querySelector(".navigation__toggle");
-/* Пункты меню*/
 var navItems = document.querySelector(".navigation__items");
 var navItemsVisible = document.querySelector(".navigation__items--visible");
-/* Изображения кнопки-бургера */
 var toggleOpen = document.querySelector(".navigation__img-toggle--open");
 var toggleClose = document.querySelector(".navigation__img-toggle--close");
+var navigation = document.querySelector(".navigation__container--not-js");
 
-/* Скрытие пунктов меню */
 navItemsVisible.classList.add("navigation__items--invisible");
 navItemsVisible.classList.remove("navigation__items--visible");
 
-/* Смена фота панели навигации при включенном JS */
 nav.classList.remove("navigation--active");
 
-/* Показ кнопки-бургера при включенном JS */
 navToggle.classList.add("navigation__toggle--js");
 
-/* Переключение видимости изображений у кнопки-бургера при включенном JS */
-toggleOpen.classList.toggle("navigation__img-toggle--invisible");
-toggleClose.classList.toggle("navigation__img-toggle--invisible");
+navigation.classList.remove("navigation__container--not-js");
+
+toggleOpen.classList.remove("navigation__img-toggle--hide");
+toggleClose.classList.add("navigation__img-toggle--hide");
 
 navToggle.addEventListener("click", function() {
   nav.classList.toggle("navigation--active");
