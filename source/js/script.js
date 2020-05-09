a2bc68a78cdd5c51d6dd23be844b4476c7575338
Сергей Архипов <s.m.arhipov@mail.ru>
2020-05-09 16:44:14 +0500

    Переделан js для меню

--- source/js/script.js
+++ source/js/script.js
@@ -1,21 +1,30 @@
-var navToggle = document.querySelector(".navigation__toggle");
+/* Панель навигации */
 var nav = document.querySelector(".navigation");
+/* Кнопка-бургер */
+var navToggle = document.querySelector(".navigation__toggle");
+/* Пункты меню*/
 var navItems = document.querySelector(".navigation__items");
 var navItemsVisible = document.querySelector(".navigation__items--visible");
-var navActive = document.querySelector(".navigation__nav-active");
+/* Изображения кнопки-бургера */
 var toggleOpen = document.querySelector(".navigation__img-toggle--open");
 var toggleClose = document.querySelector(".navigation__img-toggle--close");
 
+/* Скрытие пунктов меню */
 navItemsVisible.classList.add("navigation__items--invisible");
 navItemsVisible.classList.remove("navigation__items--visible");
-navActive.classList.remove("navigation__nav-active");
+
+/* Смена фота панели навигации при включенном JS */
+nav.classList.remove("navigation--active");
+
+/* Показ кнопки-бургера при включенном JS */
+navToggle.classList.add("navigation__toggle--js");
+
+/* Переключение видимости изображений у кнопки-бургера при включенном JS */
 toggleOpen.classList.toggle("navigation__img-toggle--invisible");
 toggleClose.classList.toggle("navigation__img-toggle--invisible");
 
-navToggle.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  navToggle.classList.toggle("navigation__toggle--active");
-  nav.classList.toggle("navigation__nav-active");
+navToggle.addEventListener("click", function() {
+  nav.classList.toggle("navigation--active");
   navItems.classList.toggle("navigation__items--visible");
   toggleOpen.classList.toggle("navigation__img-toggle--invisible");
   toggleClose.classList.toggle("navigation__img-toggle--invisible");
