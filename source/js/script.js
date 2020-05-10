89b8d999ea0e1df348d6686d3ca7a0fe5d50ea64
Сергей Архипов <s.m.arhipov@mail.ru>
2020-05-10 20:02:20 +0500

    Сделан JS для навигации

--- source/js/script.js
+++ source/js/script.js
@@ -2,25 +2,23 @@ var nav = document.querySelector(".navigation");
 var navToggle = document.querySelector(".navigation__toggle");
 var navItems = document.querySelector(".navigation__items");
 var navItemsVisible = document.querySelector(".navigation__items--visible");
-var toggleOpen = document.querySelector(".navigation__img-toggle--open");
-var toggleClose = document.querySelector(".navigation__img-toggle--close");
+var toggleOpen = document.querySelector(".navigation__toggle-open");
+var toggleClose = document.querySelector(".navigation__toggle-close");
 var navigation = document.querySelector(".navigation__container--not-js");
+var header = document.querySelector(".page-header");
 
+header.classList.remove("page-header--not-js");
 navItemsVisible.classList.add("navigation__items--invisible");
 navItemsVisible.classList.remove("navigation__items--visible");
-
 nav.classList.remove("navigation--active");
-
 navToggle.classList.add("navigation__toggle--js");
-
 navigation.classList.remove("navigation__container--not-js");
-
-toggleOpen.classList.remove("navigation__img-toggle--hide");
-toggleClose.classList.add("navigation__img-toggle--hide");
+toggleOpen.classList.remove("navigation__toggle-hide");
+toggleClose.classList.add("navigation__toggle-hide");
 
 navToggle.addEventListener("click", function() {
   nav.classList.toggle("navigation--active");
   navItems.classList.toggle("navigation__items--visible");
-  toggleOpen.classList.toggle("navigation__img-toggle--invisible");
-  toggleClose.classList.toggle("navigation__img-toggle--invisible");
+  toggleOpen.classList.toggle("navigation__toggle-hide");
+  toggleClose.classList.toggle("navigation__toggle-hide");
 });
