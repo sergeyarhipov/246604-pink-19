109c9f084da570a20795752bf630806488880a1e
Сергей Архипов <s.m.arhipov@mail.ru>
2020-05-11 16:42:44 +0500

    Сделаны обработчики событий форм

--- source/js/script.js
+++ source/js/script.js
@@ -5,7 +5,12 @@ var navItemsVisible = document.querySelector(".navigation__items--visible");
 var toggleOpen = document.querySelector(".navigation__toggle-open");
 var toggleClose = document.querySelector(".navigation__toggle-close");
 var navigation = document.querySelector(".navigation__container--not-js");
+var form = document.querySelector(".form");
 var header = document.querySelector(".page-header");
+var popupSend = document.querySelector(".popup--send");
+var popupError = document.querySelector(".popup--error");
+var popupBtnSend = document.querySelector(".popup__btn--send");
+var popupBtnError = document.querySelector(".popup__btn--error");
 
 header.classList.remove("page-header--not-js");
 navItemsVisible.classList.add("navigation__items--invisible");
@@ -21,4 +26,20 @@ navToggle.addEventListener("click", function() {
   navItems.classList.toggle("navigation__items--visible");
   toggleOpen.classList.toggle("navigation__toggle-hide");
   toggleClose.classList.toggle("navigation__toggle-hide");
+});
+
+form.addEventListener('invalid', function() {
+  popupError.classList.remove("popup--invisible");
+}, true)
+
+form.addEventListener("submit", function(){
+  popupSend.classList.remove("popup--invisible");
+});
+
+popupBtnSend.addEventListener("click", function() {
+  popupSend.classList.add("popup--invisible");
+});
+
+popupBtnError.addEventListener("click", function() {
+  popupError.classList.add("popup--invisible");
 });
